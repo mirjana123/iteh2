6d6b1455070ce15611fce363c38ed54c4ebf84a2
Mirjana Risticevic <mristicevic@gmail.com>
2021-12-27 18:22:11 +0100

    ubacivanje proizvoda 1

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -11,7 +11,7 @@
           @foreach($data as $product)
           <div class="col-md-4">
             <div class="product-item">
-              <a href="#"><img src="/assets/{{$product->image}}" alt=""></a>
+              <a href="#"><img src="/assets/images/{{$product->image}}" alt=""></a>
               <div class="down-content">
                 <a href="#"><h4>{{$product->title}}</h4></a>
                 <h6>{{$product->price}}</h6>
